eb2283d8f6a6d0302301856e387b69ea428c975f
DawidFurs <dawid.furs@onet.pl>
2024-01-10 17:58:00 +0100

    Create react app, nothing on site yet

---
+++ RestAPI_Grzyby/React_web/mushroom_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+// npm start : uruchomienie web servera
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+        Hello I am, a basic React website
+        </h1>
+    </div>
+  );
+}
+
+export default App;
