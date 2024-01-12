3f6668f07c76c3c0b09280d557642e4ba62a0d71
DawidFurs <dawid.furs@onet.pl>
2024-01-12 15:52:28 +0100

    Web form with drop boxes, Submit button responds with POST data from backend, still need fix on drop boxes (values dissapear after submission)
    For backend added GET endpoint to receive data in order to use it for drop boxes and CORS

--- RestAPI_Grzyby/React_web/mushroom_app/src/App.js
+++ RestAPI_Grzyby/React_web/mushroom_app/src/App.js
@@ -2,8 +2,7 @@ import React, {useState, useEffect} from "react"
 import api from "./api"
 
 const App = () => {
-        const [mushroom, setMushroom] = useState([]);
-        const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
         ksztalt_kapelusza: '',
         powierzchnia_kapelusza: '',
         kolor_kapelusza: '',
@@ -28,67 +27,113 @@ const App = () => {
         srodowisko: ''
     });
 
-//    const fetchMushroom = async () => {
-//        const response = await api.get('/grzyby/klasyfikuj/');
-//        setMushroom(response.data)
-//    };
-
-//    useEffect(() => {
-//        fetchMushroom();
-//    }, []);
-
-
-    const handleInputChange = (event) => {
-        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    }
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        await api.post('/grzyby/klasyfikuj/', formData);
-//        fetchMushroom();
-        setFormData({
-            ksztalt_kapelusza: '',
-            powierzchnia_kapelusza: '',
-            kolor_kapelusza: '',
-            zasinienia: '',
-            zapach: '',
-            sposob_przyrastania_blaszek: '',
-            odstep_miedzy_blaszkami: '',
-            rozmiar_blaszek: '',
-            kolor_blaszek: '',
-            ksztalt_trzonu: '',
-            korzen_trzonu: '',
-            powierzchnia_trzonu_nad_pierscieniem: '',
-            powierzchnia_lodygi_ponizej_pierscienia: '',
-            kolor_trzonu_nad_pierscieniem: '',
-            kolor_trzonu_pod_pierscieniem: '',
-            rodzaj_oslony: '',
-            kolor_oslony: '',
-            liczba_pierscieni: '',
-            rodzaj_pierscienia: '',
-            kolor_wysypu_zarodnikow: '',
-            populacja: '',
-            srodowisko: ''
-        });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/get-data");
+        const data = await response.json();
+        setFormData(data);
+      } catch (error) {
+        console.error("Błąd pobierania danych:", error);
+      }
     };
 
+    fetchData();
+  }, []);
 
-return(
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await api.post("/grzyby/klasyfikuj/", formData);
+      // Handle the response from the server (show result to the user)
+      console.log(response.data);
+    } catch (error) {
+      // Handle errors (show an error message to the user)
+      console.error("Error submitting form:", error.message);
+    }
+
+    // Clear the form after submission
+    setFormData({
+        ...formData,
+        ksztalt_kapelusza: formData.ksztalt_kapelusza,
+        powierzchnia_kapelusza: "",
+        kolor_kapelusza: '',
+        zasinienia: '',
+        zapach: '',
+        sposob_przyrastania_blaszek: '',
+        odstep_miedzy_blaszkami: '',
+        rozmiar_blaszek: '',
+        kolor_blaszek: '',
+        ksztalt_trzonu: '',
+        korzen_trzonu: '',
+        powierzchnia_trzonu_nad_pierscieniem: '',
+        powierzchnia_lodygi_ponizej_pierscienia: '',
+        kolor_trzonu_nad_pierscieniem: '',
+        kolor_trzonu_pod_pierscieniem: '',
+        rodzaj_oslony: '',
+        kolor_oslony: '',
+        liczba_pierscieni: '',
+        rodzaj_pierscienia: '',
+        kolor_wysypu_zarodnikow: '',
+        populacja: '',
+        srodowisko: ''
+    });
+  };
+
+
+
+  return (
     <div>
-        <nav className= "navbar mavbar-dark bg-primary">
-            <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-                Mushroom app
-            </a>
+      <nav className="navbar mavbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Mushroom app
+          </a>
+        </div>
+      </nav>
+      <div className="container">
+        <form onSubmit={handleFormSubmit}>
+          {Object.entries(formData).map(([key, value]) => (
+            <div key={key} className="mb-3 mt-3">
+              <label htmlFor={key}>{key}:</label>
+              <select
+                id={key}
+                name={key}
+                value={formData[key]}
+                onChange={handleInputChange}
+              >
+                <option value="">Wybierz...</option>
+                {Array.isArray(value)
+                  ? value.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))
+                  : (
+                      <option key={value} value={value}>
+                        {value}
+                      </option>
+                  )
+                }
+              </select>
             </div>
-        </nav>
-
-
-
-
+          ))}
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-)
-
-}
+  );
+};
 
 export default App;
